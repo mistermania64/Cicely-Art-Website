8565cc28237147e4af6f846de0478d1c891a4260
mistermania64 <81997874+mistermania64@users.noreply.github.com>
2025-04-13 02:28:09 -0400

    animations page changed to flex-box

--- js/javascriptFunctions.js
+++ js/javascriptFunctions.js
@@ -1,13 +1,13 @@
-function showHidden(id) 
+function showHidden(elementID, btnID) 
 {
-    if (document.getElementById(id).style.display == 'none') 
+    if (document.getElementById(elementID).style.display == 'none') 
     { 
-        document.getElementById(id).style.display = 'block';
+        document.getElementById(elementID).style.display = 'block';
     } 
     
     else 
     {
-        document.getElementById(id).style.display = 'none';
+        document.getElementById(elementID).style.display = 'none';
     }
 }
 
